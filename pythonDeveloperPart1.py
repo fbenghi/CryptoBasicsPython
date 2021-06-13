fb4143cae74f287c0d3f0a4df71f8a71282900cb
Felipe Benghi <fbenghi@gmail.com>
2021-06-13 11:16:15 -0300

    Add calls to basic contract interactions

--- pythonDeveloperPart1.py
+++ pythonDeveloperPart1.py
@@ -15,7 +15,8 @@ Web3.fromWei(1, 'gwei')
 
 # %%
 # conectando no nó fake
-w3 = Web3(Web3.EthereumTesterProvider())
+# w3 = Web3(Web3.EthereumTesterProvider())
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 
 # %%
 # Verificando conexão
@@ -52,4 +53,33 @@ w3.eth.waitForTransactionReceipt(tx_hash)
 Web3.fromWei(w3.eth.getBalance(w3.eth.accounts[0]), 'ether')
 # %%
 Web3.fromWei(w3.eth.getBalance(w3.eth.accounts[1]), 'ether')
+# %%
+# Contract interaction
+w3.eth.getTransactionReceipt('0x2c35813d4ec6aa1f346008273489c6ecefbbddeceb8ec19b364c6b2a7cf415e6')
+
+# %%
+# Contract creation
+w3.eth.getTransactionReceipt('0xb9a40a1e49af1026b51044727a12c3e2bf88d6120dbf4e912df100fe732e4342')
+# %%
+#load ERC contract
+contractAddress = '0x26760F09b813C49B731837ab2dF23e3b82f0dfDc'
+
+import json
+with open('/home/felipe/Documents/Ethereum/Scripts/Remix-Ide/Contracts/Contracts/artifacts/FucksToken.json') as json_file:
+    contractData = json.load(json_file)
+
+
+contract = w3.eth.contract(contractAddress, abi=contractData["abi"])
+
+# %%
+contract.functions.name().call()
+# %%
+contract.functions.symbol().call()
+# %%
+decimals = contract.functions.decimals().call()
+# %%
+contract.functions.totalSupply().call()
+# %%
+contractOwner = w3.eth.accounts[9]
+contract.functions.balanceOf(contractOwner).call()
 # %%
